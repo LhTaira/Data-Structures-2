8416529cce85541e5a57e78d65cd684e2e7d3ec9
Luis Taira <luishptaira@gmail.com>
2019-12-04 00:00:46 -0300

    Making progress in natureza.c

--- trabalho2/natureza.c
+++ trabalho2/natureza.c
@@ -2,38 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Heap {
-    struct Heap * next;
+#define vertex int
+
+// typedef struct node *link;
+// struct node { 
+//    vertex w; 
+//    link next; 
+// };
+
+typedef struct List {
+    struct List * next;
     int number;
-} Heap;
+} List;
 
 typedef struct Graph {
-    Heap * heap;
-    Heap * heapEnd;
+    List * list;
+    List * heapEnd;
     char name[31];
 } Graph;
 
-typedef struct Edge {
-    int n1, n2;
-} Edge;
+// typedef struct Edge {
+//     int n1, n2;
+// } Edge;
 
-Edge EDGE(int, int);
+// Edge EDGE(int a, int b) {
+//     Edge c;
+//     c.n1 = a;
+//     c.n2 = b;
+//     return c;
+// }
 
 void createHeap(Graph * graph, int number) {
-    graph->heap = malloc(sizeof(Heap));
-    graph->heap->next = NULL;
-    graph->heap->number = number;
-    graph->heapEnd = graph->heap;
+    graph->list = malloc(sizeof(List));
+    graph->list->next = NULL;
+    graph->list->number = number;
+    graph->heapEnd = graph->list;
 }
 
 void push(Graph * graph, int number) {
-    Heap * newNode;
+    List * newNode;
 
-    if(graph->heap == NULL) {
+    if(graph->list == NULL) {
         return createHeap(graph, number);
     }
 
-    newNode = malloc(sizeof(Heap));
+    newNode = malloc(sizeof(List));
     newNode->next = NULL;
     newNode->number = number;
 
@@ -56,12 +69,12 @@ void makeLink(Graph * graph, char * predador, char * presa) {
 }
 
 void printGraph(Graph * graph, int nNomes) {
-    Heap * aux;
+    List * aux;
 
     for(int i = 0; i < nNomes; i++) {
         printf("%s", graph[i].name);
 
-        aux = graph[i].heap;
+        aux = graph[i].list;
         while(aux != NULL) {
             printf(" %d", aux->number);
             aux = aux->next;
@@ -71,9 +84,9 @@ void printGraph(Graph * graph, int nNomes) {
 }
 
 void freeEverything(Graph * graph, int nNomes) {
-    Heap * aux, * aux2;
+    List * aux, * aux2;
     for(int i = 0; i < nNomes; i++) {
-        aux = graph[i].heap;
+        aux = graph[i].list;
         while(aux != NULL) {
             aux2 = aux->next;
             free(aux);
@@ -83,14 +96,31 @@ void freeEverything(Graph * graph, int nNomes) {
     free(graph);
 }
 
-void dfsR(Graph graph, Edge edge, int * pre, int counter;) {
-    int v = edge.n1;
-    pre[v] = counter++;
+int getItem(Graph graph, int index) {
+    while(graph.list != NULL) {
+        if(graph.list->number == index) {
+            return 1;
+        }
+        graph.list = graph.list->next;
+    }
+    return -1;
+}
 
-    for(int t = 0; t < graph)
+void dfsR(Graph * graph, int source, int destination, int * pre, int * counter, int nVertices) {
+    pre[source] = (*counter)++;    
+
+    for(int t = 0; t < nVertices; t++) {
+        if(getItem(graph[source], t) != -1) {
+            if(pre[t] == -1) {
+                dfsR(graph, t, destination, pre, counter, nVertices);
+            }
+        }
+    }
 }
 
 int main() {
+    int * pre, * counter = malloc(sizeof(int)), maior;
+
     Graph * graph;
     int nNomes, nCadeias;
     char predador[31], presa[31];
@@ -98,10 +128,13 @@ int main() {
     scanf("%d %d", &nNomes, &nCadeias);
 
     while(nNomes != 0 && nCadeias != 0) {
-
+        
+        pre = malloc(nNomes * sizeof(int));
         graph = malloc(nNomes * sizeof(Graph));
+        maior = -1;
+
         for(int i = 0; i < nNomes; i++) {
-            graph[i].heap == NULL;
+            graph[i].list == NULL;
         }
 
         for(int i = 0; i < nNomes; i++) {
@@ -113,10 +146,27 @@ int main() {
             makeLink(graph, predador, presa);
         }
 
-        // printGraph(graph, nNomes);
+        for(int i = 0; i < nNomes; i++) {
+            for(int j = 0; j < nNomes; j++) {
+                pre[j] = -1;
+            }
+            for(int j = 0; j < nNomes; j++) {
+                *counter = 0;
+                dfsR(graph, i, j, pre, counter, nNomes);
+            }
+            for(int j = 0; j < nNomes; j++) {
+                if(pre[j] > maior) {
+                    maior = pre[j];
+                }
+            // printf("gay\n");
+            }
+        }
 
-        freeEverything(graph, nNomes);
+        printf("%d\n", maior + 1);
 
+        // freeEverything(graph, nNomes);
+        free(pre);
+        // scanf("%c");
         scanf("%d %d", &nNomes, &nCadeias);
     }
 }
